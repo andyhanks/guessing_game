0251affd3ccbd5d67a7fa85bc40de4fe35791c85
Andrew Hanks <andymchanks@gmail.com>
2023-12-06 12:00:11 -0500

    Phase 1 complete!

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Numerics;
+//Step 1
+Console.WriteLine("Guess the secret number!");
+Console.WriteLine("------------------------");
+Console.WriteLine("");
+
+
+// step 3
+string answer = Console.ReadLine();
+
+//step 4 
+Console.WriteLine($@"
+You guessed " + answer +"!");
