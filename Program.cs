60f519b75292798c6af9f7326c1f3df4390b2771
Andrew Hanks <andymchanks@gmail.com>
2023-12-06 14:43:47 -0500

    Phase 2 complete Compares user input to 42

--- Program.cs
+++ Program.cs
@@ -1,14 +1,42 @@
 ﻿using System;
-using System.Numerics;
-//Step 1
+using System.ComponentModel.DataAnnotations;
+
+//Step 1 Intro 
 Console.WriteLine("Guess the secret number!");
 Console.WriteLine("------------------------");
 Console.WriteLine("");
 
 
-// step 3
-string answer = Console.ReadLine();
+// step 2 User input
+Console.WriteLine("Enter your guess: ");
 
-//step 4 
-Console.WriteLine($@"
-You guessed " + answer +"!");
+// step 3 save users selection as a variable
+string userAnswer = Console.ReadLine();
+int parsedUserGuessString = int.Parse(userAnswer);
+
+
+//step 4 Display users selection
+// Console.WriteLine($@"
+// You guessed " + parsedUserGuessString+"!");
+
+//Phase 2 Step 1
+int secretNumber = 42;
+
+// Compare user guess with secret number
+void compareSecret(int parsedUserGuessString)
+{
+
+
+
+    if (parsedUserGuessString == secretNumber)
+    {
+               Console.Write("Success!");
+    }
+    else {
+        Console.Write("Bummer :(");
+    }
+
+}
+
+compareSecret(parsedUserGuessString);
+
